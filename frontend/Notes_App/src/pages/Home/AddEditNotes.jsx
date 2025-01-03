09dda2030b6c482e55011d05a32f466d839f3438
Khwahish <khwahishvaid07@gmail.com>
2025-01-03 17:41:52 -0500

    Add Edit Notes page designing with routing

--- frontend/Notes_App/src/pages/Home/AddEditNotes.jsx
+++ frontend/Notes_App/src/pages/Home/AddEditNotes.jsx
@@ -1,16 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, {useState} from 'react'
 import TagInput from '../../components/input/TagInput'
+import { MdClose } from 'react-icons/md'
 
-const AddEditNotes = () => {
+const AddEditNotes = ({onClose}) => {
+
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [tags, setTags] = useState([])
+
   return (
-    <div>
+    <div className='relative'>
+
+        <button className='w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-500' 
+            onClick={onClose}>
+            <MdClose className='text-xl text-slate-400'/>
+        </button>
         <div className="flex flex-col gap-2">
         <label className="input-label">TITLE</label>
         <input
             type="text"
             className="text-2xl text-slate-950 outline-none"
             placeholder="Go To Gym At 5"
+            value={title}
+            onChange={({target}) => setTitle(target.value)}
         />
         </div>
         <div className='flex flex-col gap-2 mt-4'>
@@ -20,12 +33,14 @@ const AddEditNotes = () => {
                 className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
                 placeholder="Content"
                 rows={10}
+                value={content}
+                onChange={({target}) => setContent(target.value)}
             />
         </div>
 
         <div className='mt-3'>
             <label className="input-label">TAGS</label>
-            <TagInput/>
+            <TagInput tags={tags} setTags={setTags}/>
         </div>
 
         <button className='btn-primary font-medium mt-5 p-3' onClick={()=>{}}>
