e99e3ac5a0cad87b36baeee40d08d2ab8426f262
Khwahish <khwahishvaid07@gmail.com>
2025-01-03 14:50:12 -0500

    Dashboard

--- frontend/Notes_App/src/pages/Home/Home.jsx
+++ frontend/Notes_App/src/pages/Home/Home.jsx
@@ -2,24 +2,31 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../components/Cards/NoteCard'
+import { MdAdd } from 'react-icons/md'
 
 const Home = () => {
   return (
     <>
+
         <Navbar />
-        <div className='container mx-auto'>
-            <NoteCard
-                title="Meeting on 7th April"
-                date="3rd Apr 2024"
-                content="Meeting on 7th April Meeting on 7th April"
-                tags="#Meeting"
-                isPinned={true}
-                onEdit={()=>{}}
-                onDelete={()=>{}}
-                onPinNote={()=>{}}
-            />
+        <div className='container mx-auto px-5'>
+            <div className='grid grid-cols-3 gap-4 mt-8' >
+                <NoteCard
+                    title="Meeting on 7th April"
+                    date="3rd Apr 2024"
+                    content="Meeting on 7th April Meeting on 7th April"
+                    tags="#Meeting"
+                    isPinned={true}
+                    onEdit={()=>{}}
+                    onDelete={()=>{}}
+                    onPinNote={()=>{}}
+                />                
+            </div>
         </div>
-    
+
+        <button className='w-14 h-14 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={()=>{}}>
+          <MdAdd className='text-[32px] text-white'/>  
+        </button>
     </>
   )
 }
