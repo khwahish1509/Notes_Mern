251c088957c1181a4360084474505a82211be187
Khwahish <khwahishvaid07@gmail.com>
2025-01-02 22:58:59 -0500

    Continuing Login

--- frontend/Notes_App/src/App.jsx
+++ frontend/Notes_App/src/App.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Home from './pages/Home/Home'
+import React from 'react';
+// Libraries for routing
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+import Login from './pages/Login/Login';
 
+// Sets up navigation for the app, linking URL paths to components
+// For example, visiting "/dashboard" shows the Home component.
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
